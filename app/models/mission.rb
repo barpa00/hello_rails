a3b40a39dc0b4be4ac97c5e85894152890b2af0e
jjn <jjn@5xruby.com>
2022-02-26 23:51:02 +0800

    step_23

--- app/models/mission.rb
+++ app/models/mission.rb
@@ -1,11 +1,24 @@
 class Mission < ApplicationRecord
   belongs_to :user
+  has_many :missions_tags
+  has_many :tags, through: :missions_tags
+
   paginates_per 10
   
   validates :title, presence: { message: I18n.t('title.error') }
   default_scope { order(created_at: :DESC) }
   enum status: { norush: 0, medium: 1, priority: 2 }
   
+  def all_tags=(names)
+    self.tags = names.split(',').map do |name|
+      Tag.find_or_create_by(name: name)
+    end
+  end
+
+  def all_tags
+    tags.map(&:name).join(',')
+  end
+
   include AASM
   aasm do 
     state :doing, initial: true
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_02_26_085349) do
+ActiveRecord::Schema[7.0].define(version: 2022_02_26_132033) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -24,6 +24,19 @@ ActiveRecord::Schema[7.0].define(version: 2022_02_26_085349) do
     t.integer "status", default: 0
     t.string "aasm_state"
     t.integer "user_id", null: false
+  end
+
+  create_table "missions_tags", force: :cascade do |t|
+    t.integer "tag_id", null: false
+    t.integer "mission_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "tags", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
