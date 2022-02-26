873d8c6b229613c5d717ad00c872eb782f72e24d
jjn <jjn@5xruby.com>
2022-02-26 21:10:46 +0800

    step_22

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,12 @@ class User < ApplicationRecord
   validates :password, presence: true, confirmation: true
 
   before_create :encrypt_password
+  before_destroy :check_admin_num
 
   has_many :missions, autosave: true, dependent: :destroy
+
+  enum roles: { user: 0, admin: 1 }
+
   def self.login(user)
     pw = encrypt_pw(user[:password])
     User.find_by(email: user[:email], password: pw)
@@ -19,4 +23,12 @@ class User < ApplicationRecord
   def self.encrypt_pw(pwd)
     Digest::SHA1.hexdigest(pwd)
   end
+
+  def check_admin_num
+    return if self.user?
+    return if User.admin.size > 1
+
+    throw :abort
+  end
 end
+ 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_02_25_103822) do
+ActiveRecord::Schema[7.0].define(version: 2022_02_26_085349) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -31,6 +31,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_02_25_103822) do
     t.string "password"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "roles", default: 0
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
